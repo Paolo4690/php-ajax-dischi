5161688cc049fc3d4b888f98dc4896e719b80d65
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-04-12 16:49:53 +0200

    add select autore e genere

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ const app = new Vue({
     el: "#app",
     data: {
       arrAlbum: [],
+      arrGenere: [],
+      arrAuthor: [],
+      genreSelect: '',
+      authorSelect: '',
     },
     created() {
-      axios.get("./db/data.php").then((res) => {
+      axios.get("./db/data2.php").then((res) => {
         this.arrAlbum = res.data;
+        this.arrGenere = [...new Set(this.arrAlbum.map(elemento => elemento.genre))]
+        this.arrAuthor = [...new Set(this.arrAlbum.map(elemento => elemento.author))]
       });
     },
+    computed: {
+      filterArray () {
+        return this.arrAlbum.filter(obj => obj.genre.includes(this.genreSelect) && obj.author.includes(this.authorSelect))
+      }
+    }
   });
